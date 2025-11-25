eeb5b74a35198a1cc0f2fe4de7219b29481aba82
James Bensley <jwbensley@gmail.com>
2025-11-25 18:12:23 +0100

    Start to parse a single MRT entry

---
+++ src/http.rs
@@ -0,0 +1,36 @@
+pub mod http {
+    use log::info;
+    use reqwest::blocking;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::Path;
+
+    pub fn download_file(url: &str, dest: &Path) {
+        if dest.exists() {
+            info!(
+                "Not GETting URL {}, output file already exists {}",
+                url,
+                dest.to_str().unwrap(),
+            );
+            return;
+        }
+
+        info!("GET'ing URL {}", url);
+
+        let response = blocking::get(url)
+            .map_err(|e| format!("HTTP GET failed: {}", e))
+            .unwrap();
+        let content = response
+            .bytes()
+            .map_err(|e| format!("Failed to read response bytes: {}", e))
+            .unwrap();
+
+        File::create(&dest)
+            .map_err(|e| format!("Failed to create file: {}", e))
+            .unwrap()
+            .write_all(&content)
+            .unwrap();
+
+        info!("Wrote to file {}", dest.to_str().unwrap());
+    }
+}
---
+++ src/ribs.rs
@@ -0,0 +1,69 @@
+pub mod ribs {
+    use crate::http::http;
+    use bgpkit_broker::BgpkitBroker;
+    use log::info;
+    use std::fs;
+    use std::path::Path;
+
+    #[derive(Debug)]
+    pub struct RibFile {
+        url: String,
+        pub filename: String,
+    }
+
+    pub fn download_ribs_for_day(date: &str, dir: &str) -> Vec<RibFile> {
+        /*
+         * Download all the ribs files for a specific day
+         */
+        let rib_files = get_rib_list_for_day(date);
+        download_ribs_to_dir(dir, &rib_files);
+        return rib_files;
+    }
+
+    fn download_ribs_to_dir(dir: &str, rib_files: &Vec<RibFile>) {
+        let mrt_path = Path::new(dir);
+        if !mrt_path.exists() {
+            info!("Creating path: {}", mrt_path.to_str().unwrap());
+            fs::create_dir(mrt_path).unwrap();
+        }
+
+        for rib_file in rib_files {
+            let dest = mrt_path.join(&rib_file.filename);
+            http::download_file(&rib_file.url, &dest)
+        }
+    }
+
+    fn get_rib_list_for_day(date: &str) -> Vec<RibFile> {
+        /*
+         * Return a list of RIBs for a specific day
+         */
+
+        let broker = BgpkitBroker::new().ts_start(date).ts_end(date);
+        let ribs = broker.daily_ribs().unwrap();
+        info!("Found {} MRT files for date {}", ribs.len(), date,);
+
+        let mut rib_files = Vec::<RibFile>::new();
+        for rib in ribs {
+            let basename = Path::new(&rib.url).file_name().unwrap().to_str().unwrap();
+
+            let source = if rib.collector_id.starts_with("rrc") {
+                String::from("ris")
+            } else {
+                String::from("route-views")
+            };
+
+            let filename = if rib.collector_id.starts_with(&source) {
+                format!("{}.{}", rib.collector_id, basename)
+            } else {
+                format!("{}.{}.{}", &source, rib.collector_id, basename)
+            };
+
+            rib_files.push(RibFile {
+                url: rib.url,
+                filename: filename,
+            });
+        }
+
+        return rib_files;
+    }
+}
