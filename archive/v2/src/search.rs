68db9a20c039182e32da569b4cc8644855cd4e9f
James Bensley <jwbensley@gmail.com>
2026-03-24 20:52:17 +0100

    Remove old archive

--- archive/v2/src/search.rs
+++
@@ -1,8 +0,0 @@
-pub mod path_search {
-
-    use crate::mrt_paths::path_data::PathData;
-
-    pub fn find_paths(path_data: &PathData) {
-        path_data.find_origins_with_divergent_paths();
-    }
-}
