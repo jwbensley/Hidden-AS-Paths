e40755bb7eabc62d8b35a6f766fcd937963512bc
James Bensley <1025875+jwbensley@users.noreply.github.com>
2026-04-18 15:27:09 +0200

    Merge these two scripts into one

--- scripts/get_irr_asns.py
+++ scripts/get_irr_asns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S uv run --script
 #
 # /// script
-# requires-python = ">=3.12"
+# requires-python = "==3.13"
 # dependencies = [
 # "requests==2.32.5",
 # ]
@@ -10,22 +10,70 @@
 import argparse
 import json
 import logging
-from typing import Any
+from typing import Any, Optional
 import requests
+import sqlite3
 
 
-def get_asset_asns(asset: str, url: str) -> list[int]:
+def get_asn_asset(asn: int, db_cursor: sqlite3.Cursor) -> list[str]:
+    logging.debug(f"Going to get data for ASN {asn}")
+
+    db_cursor.execute(
+        f"SELECT irr_as_set FROM peeringdb_network WHERE asn = {asn}"
+    )
+    row = db_cursor.fetchone()
+
+    if not row:
+        logging.error(f"No data found for ASN {asn}")
+        return [""]
+    asset: str = row[0]
+
+    if not asset:
+        logging.error(f"No AS-SET found for ASN {asn}")
+        return [""]
+
+    if len(asset.split()) == 1:
+        # logging.info(f"AS-SET for ASN {asn} is {asset}")
+        return [asset.split("::")[-1]]
+
+    parts = asset.split()
+    logging.warning(f"AS-SET for ASN {asn} contains multiple parts: {asset}")
+    return [part.split("::")[-1] for part in parts]
+
+
+def get_asn_assets(asns: list[int], db_path: str) -> dict[int, list[str]]:
+    assets: dict[int, list[str]] = {}
+    db_conn: sqlite3.Connection = sqlite3.connect(db_path)
+    db_cursor: sqlite3.Cursor = db_conn.cursor()
+    for asn in asns:
+        assets[asn] = get_asn_asset(asn, db_cursor)
+    db_conn.close()
+    return assets
+
+
+def get_asset_asns(
+    asset: str, url: str, session: Optional[requests.Session] = None
+) -> list[int]:
     logging.debug(f"Getting ASNs for AS-SET {asset} from IRRd server at {url}")
+
+    rpsl_key = asset.split("::")[-1]
+    if rpsl_key.upper().startswith("RS-"):
+        logging.warning(f"Set {asset} looks like a route set, skipping")
+        return []
 
     query = f"""
     query {{
-        recursiveSetMembers(setNames: ["{asset}"]) {{
+        recursiveSetMembers(setNames: ["{rpsl_key}"]) {{
             rpslPk, members
         }}
     }}
     """
 
-    response = requests.post(f"{url}/graphql/", json={"query": query})
+    if session:
+        response = session.post(f"{url}/graphql/", json={"query": query})
+    else:
+        response = requests.post(f"{url}/graphql/", json={"query": query})
+
     response.raise_for_status()
     assert response.status_code == 200
 
@@ -40,32 +88,42 @@ def get_asset_asns(asset: str, url: str) -> list[int]:
     return members
 
 
-def get_asset_asns_for_assets(
+def get_asns_for_assets(
     asn_assets: dict[int, list[str]], url: str
 ) -> dict[int, list[int]]:
     member_asns: dict[int, list[int]] = {}
+    session = requests.Session()
+
     for asn, assets in asn_assets.items():
         unique_asns: set[int] = set()
         for asset in assets:
             if not asset:
                 logging.warning(f"Skipping missing AS-SET for ASN {asn}")
                 continue
-            unique_asns.update(get_asset_asns(asset, url))
+            unique_asns.update(get_asset_asns(asset, url, session))
         member_asns[asn] = list(unique_asns)
     return member_asns
 
 
-def load_asn_assets(input_file: str) -> dict[int, list[str]]:
+def load_input_asns(input_file: str) -> list[int]:
     with open(input_file, "r") as f:
-        data: dict[int, list[str]] = json.load(f)
+        data: list[list[int]] = json.load(f)
 
-    logging.info(f"Loaded AS-SETs for {len(data)} ASNs")
-    return data
+    logging.info(f"Loaded {len(data)} ASNs")
+    return [asn for (asn, _) in data]
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="Get all the ASNs that are in the IRR AS-SET for each ASN in the input list"
+        description="Get all the ASNs that are in the IRR AS-SET for each ASN in the input list",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--db",
+        "-b",
+        type=str,
+        help="Path to the SQLite database file containing PeeringDB data",
+        default="peeringdb/peeringdb.sqlite3",
     )
     parser.add_argument(
         "--debug",
@@ -78,8 +136,8 @@ def parse_args() -> argparse.Namespace:
         "--input",
         "-i",
         type=str,
-        help="Input JSON file containing list of ASNs and their AS-SETs",
-        default="results/asn_assets.json",
+        help="Path to the input JSON file containing ASNs",
+        default="results/diverging_asn_count.json",
     )
     parser.add_argument(
         "--output",
@@ -118,8 +176,9 @@ def write_json(filename: str, data: dict[Any, Any]) -> None:
 
 def main():
     args = parse_args()
-    asn_assets = load_asn_assets(args.input)
-    irr_asns = get_asset_asns_for_assets(asn_assets, args.url)
+    asns = load_input_asns(args.input)
+    assets = get_asn_assets(asns, args.db)
+    irr_asns = get_asns_for_assets(assets, args.url)
     write_json(args.output, irr_asns)
 
 
